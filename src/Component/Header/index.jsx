0dc6ed16d201e99e24e2c7a7024e9bd8ac3290dd
Olayiwola Olanrewaju <larry_coal@outlook.com>
2023-10-20 08:17:15 -0400

    mobile responsiveness

--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import headerLogo from "../../assets/MainLogo.svg";
+import hamburger from "../../assets/hamburger.svg";
+import closeBtn from "../../assets/close.svg";
 import "./index.css";
 import { Route, Link, useLocation } from "react-router-dom";
 import { useEffect } from "react";
@@ -7,6 +9,7 @@ import { useState } from "react";
 const Header = () => {
   const location = useLocation();
   const [currLocation, setCurrLocation] = useState("home");
+  const [showSideBar, setShowSideBar] = useState(false);
   useEffect(() => {
     switch (location.pathname) {
       case "/":
@@ -26,12 +29,50 @@ const Header = () => {
     }
   }, [location]);
   return (
-    <div className="header_wrapper" onClick={() => console.log("clicked")}>
-      <img src={headerLogo} alt="header logo" className="header_logo" />
-      <div className="horizontal_line-wrapper">
-        <div className="horizontal_line"></div>
+    <>
+      <div className="header_wrapper" onClick={() => console.log("clicked")}>
+        <img src={headerLogo} alt="header logo" className="header_logo" />
+        <div className="horizontal_line-wrapper">
+          <div className="horizontal_line"></div>
+        </div>
+        <div className="tabs">
+          <ul>
+            <li className={currLocation === "home" ? "active" : ""}>
+              <Link to="/">
+                <span>00</span> <span>HOME</span>
+              </Link>
+            </li>
+            <li className={currLocation === "destination" ? "active" : ""}>
+              <Link to="/destination">
+                <span>01</span> <span>DESTINATION</span>
+              </Link>
+            </li>
+            <li className={currLocation === "crew" ? "active" : ""}>
+              <Link to="/crew">
+                <span>02</span> <span>CREW</span>
+              </Link>
+            </li>
+            <li className={currLocation === "technology" ? "active" : ""}>
+              <Link to="/technology">
+                <span>03</span> <span>TECHNOLOGY</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div
+          className="hamburger"
+          onClick={() => setShowSideBar(!showSideBar)}
+        >
+          <img src={hamburger} alt="hamburger logo" />
+        </div>
       </div>
-      <div className="tabs">
+      <div
+        className="sidebar"
+        style={{ display: showSideBar ? "flex" : "", width:showSideBar && "300px" }}
+      >
+        <div className="close_btn" onClick={() => setShowSideBar(!showSideBar)}>
+          <img src={closeBtn} alt="close botton" />
+        </div>
         <ul>
           <li className={currLocation === "home" ? "active" : ""}>
             <Link to="/">
@@ -55,7 +96,7 @@ const Header = () => {
           </li>
         </ul>
       </div>
-    </div>
+    </>
   );
 };
 
